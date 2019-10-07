8a5dc1597055c0de13a0ceb2be2af7147d190315
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-07 18:55:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(seconds)
   end
   "HAPPY NEW YEAR"
 end
+
+def countdown_with_sleep(seconds)
+  
+  while seconds > 0 
+    puts "#{seconds} SECOND(S)"
+    seconds -= 1
+    sleep
+  end
+  "HAPPY NEW YEAR"
+end
